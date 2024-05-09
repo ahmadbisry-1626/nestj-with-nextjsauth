29dfe9b911a836217c9d76408689a42c5cefb734
16_shrr <ctbaby1626@gmail.com>
2024-05-09 22:33:38 +0700

    Add tailwind.config.ts and cn.ts files, update package.json and layout.tsx, create Navbar.tsx and TextGenerate.tsx components

---
+++ src/app/components/Navbar.tsx
@@ -0,0 +1,26 @@
+import React from 'react'
+import { ButtonKeren } from './ui/ButtonKeren'
+
+const Navbar = () => {
+    return (
+        <nav className='w-full fixed bg-[#09090b]'>
+            <div className='wrapper flex items-center justify-between'>
+                <div className='flex items-end gap-1 text-gray-50'>
+                    <h1 className='text-2xl font-semibold tracking-wide'>NestJS</h1>
+                    <span className='text-md'>&NextJS</span>
+                </div>
+
+                <div className='flex items-center gap-2'>
+                    <ButtonKeren>
+                        Sign in
+                    </ButtonKeren>
+                    <ButtonKeren>
+                        Sign up
+                    </ButtonKeren>
+                </div>
+            </div>
+        </nav>
+    )
+}
+
+export default Navbar
